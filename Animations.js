292a647f14d418cac6863872aa9a2bd9cd163882
Nicholas Canchola <72483681+cancholan@users.noreply.github.com>
2023-05-18 12:08:36 -0400

    Add FAQ accordion

--- Animations.js
+++ Animations.js
@@ -8,3 +8,12 @@ setInterval(function() {
     input.placeholder =  artistArr[index];
     index = (index+1)%numArtists;
 }, 2000);
+
+const accordion = document.getElementsByClassName('acc-container');
+
+for (let i=0; i < accordion.length; i++){
+    console.log(accordion[i]);
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('acc-active');
+    })
+}
