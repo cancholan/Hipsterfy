2b293755ae7761de4d82ddad76ffe630dc5e9208
Nicholas Canchola <72483681+cancholan@users.noreply.github.com>
2023-07-18 16:01:22 -0400

    Update Animations.js
    - change anon function to arrow

--- Animations.js
+++ Animations.js
@@ -13,7 +13,7 @@ var numArtists = artistArr.length;
 var index = 0;
 var input = document.querySelector("#textInput");
 
-setInterval(function () {
+setInterval(() => {
   input.placeholder = artistArr[index];
   index = (index + 1) % numArtists;
 }, 2000);
@@ -22,7 +22,7 @@ setInterval(function () {
 const accordion = document.getElementsByClassName("acc-container");
 
 for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function () {
+  accordion[i].addEventListener("click", function() {
     this.classList.toggle("acc-active");
   });
 }
